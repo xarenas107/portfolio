b8fffcedf58c92d2b57e368ce24e4dd1b8c913c7
Xavier <xarenas107@gmail.com>
2024-11-14 14:11:04 -0500

    refactor:  mousemove rotation event

--- app/components/hero/image.vue
+++ app/components/hero/image.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div(class='relative overflow-visible w-full lg:order-1 lg:max-w-lg lg:h-[60dvh] lg:max-h-dvh' ref='image')
+div(class='relative overflow-visible w-full lg:order-1 lg:max-w-lg lg:h-[60dvh] lg:max-h-dvh parallax-container' ref='image')
     u-lazy(:transition='{ name: "transition-grow-y" }' :dealy='100' class='w-full h-full pointer-events-none')
         div(key='bg-hero' class='w-full h-full min-h-[30dvh] sm:min-h-[40dvh] rounded-lg bg-primary-500 dark:bg-primary-600 relative overflow-visible z-20  lg:rounded-b-[50%]')
             //- bg-gradient(class='mix-blend-multiply')
@@ -24,7 +24,8 @@ const reduceMotion = usePreferredReducedMotion()
 const target = useTemplateRef('image')
 
 const state = {
-    active: false
+    active: false,
+    smooth: 0.07
 }
 
 const round = (value: number) => Math.round(Math.max(Math.min(value, 5), -5))
@@ -35,20 +36,25 @@ const on = {
 
         const element = target.value
         const { width = 0, height = 0 } = element?.getBoundingClientRect() || {}
+        const { offsetX, offsetY } = event
         
         const move = {
-            x:  Math.round((width / 2) - event.offsetX),
-            y:  Math.round((height / 2) - event.offsetY),
+            x:  Math.round((width / 2) - offsetX) * state.smooth ,
+            y:  Math.round((height / 2) - offsetY) * -state.smooth,
             z: 0
         }
 
         if (element) {
-            element.style.transformStyle = 'preserve-3d'
+            // element.style.transformStyle = 'preserve-3d'
 
-            move.z = round((move.x + move.y) / 10)
+            move.z = round((move.x + move.y) * state.smooth / 2)
 
-            element.style.transform = `rotateY(${move.x / 10}deg) rotateX(${move.y / 10}deg) rotateZ(${move.z}deg) perspective(550px)`
-            element.style.transition = 'transform ease .5s'
+            // element.style.transform = `rotateY(${move.x / 50}deg) rotateX(${move.y / 10}deg) rotateZ(${move.z}deg) perspective(550px)`
+            // element.style.transition = 'transform ease .5s'
+
+            element.style.setProperty('--rotate-x', `${move.x}deg`)
+            element.style.setProperty('--rotate-y', `${move.y}deg`)
+            element.style.setProperty('--rotate-z', `${move.z}deg`)
         }
 
         if (!state.active) useEventListener(target, 'mouseleave', on.leave, { once: true })
@@ -56,7 +62,13 @@ const on = {
     }),
     leave: () => {
         const element = target.value
-        if (element) element.style.transform = ''
+        // if (element) element.style.transform = ''
+
+        if (element) {
+            element.style.setProperty('--rotate-x', `0deg`)
+            element.style.setProperty('--rotate-y', `0deg`)
+            element.style.setProperty('--rotate-z', `0deg`)
+        }
         state.active = false
     }
 }
@@ -70,4 +82,16 @@ useEventListener(target, 'mousemove', on.mousemove)
     transform-style: preserve-3d;
     perspective: 550px;
 }
+
+.parallax-container {
+  transform: rotateX(var(--rotate-x)) rotateY(var(--rotate-y)) rotateZ(var(--rotate-z)) perspective(550px);
+  transform-style: preserve-3d;
+  transition: transform 1.5s cubic-bezier(0.05, 0.5, 0, 1);
+  will-change: transform;
+
+  &>:last-child {
+    transform: translateZ(-3rem)
+  }
+}
+
 </style>
