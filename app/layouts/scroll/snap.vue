c3d458a8b269d48fc4a56b2308755962679f088d
Xavier <xarenas107@gmail.com>
2024-11-26 04:01:24 -0500

    feat: add `scroll-snap` layout

---
+++ app/layouts/scroll/snap.vue
@@ -0,0 +1,11 @@
+<template lang='pug'>
+div(class='snap-y snap-mandatory h-dvh w-full overflow-auto scroll-smooth')
+    slot(class='snap-start ')
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
