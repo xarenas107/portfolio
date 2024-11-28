330ef46345b6b6e2fae0da43360a6e3b8fc07c6b
Xavier <xarenas107@gmail.com>
2024-11-28 04:36:40 -0500

    fix(heading): solve strange spacing before animation start in lg devices

--- app/components/beta/section/hero.vue
+++ app/components/beta/section/hero.vue
@@ -4,45 +4,43 @@ div(class='bg-primary-100/20 dark:bg-primary-900/20 max-w-full overflow-clip fle
 		u-container(class="flex h-full w-full flex-col gap-4 overflow-visible py-32 relative")
 			//- bg-gradient(class='fixed top-0 left-0 w-full h-full mix-blend-multiply dark:mix-blend-screen opacity-80')
 
-			u-transition(delay='1s' before-enter-class="max-w-0" enter-class='max-w-full' duration='1s' timing-function="ease-in-out")
+			u-transition(delay='1s' before-enter-class="max-w-0" enter-class='max-w-full' duration='.75s' timing-function="ease-in-out")
 				template(#default='{ state, ...props }')
 					div(class='flex justify-center gap-4 transition-all duration-200')
-						h6(:class='ui.text.base' class='font-display uppercase font-bold text-2xl max-w-prose sm:text-4xl lg:text-6xl leading-4 text-nowrap') {{ t('greetings.hi') }}
+						//- h6(:class='ui.text.base' class='font-display uppercase font-bold text-2xl max-w-prose sm:text-4xl lg:text-6xl leading-4 text-nowrap') {{ t('greetings.hi') }}
+						h6(:class='ui.text.base' class='font-display uppercase font-bold text-2xl max-w-prose sm:text-4xl lg:text-6xl leading-4 text-nowrap') {{ greetings }}
 						u-divider(v-bind='props' :ui='ui.divider' size='md')
-
-			//- u-transition(delay='.5s' before-enter-class="scale-y-0" active-class="origin-bottom" duration='.5s')
-			//- 	template(#default='props')
-			//- 		beta-hero-image(v-bind='props' class='lg:order-1 lg:hidden flex')
 
 			u-transition(delay='.5s' active-class='origin-bottom' before-enter-class="max-h-0" enter-class='max-h-48' duration='.5s' timing-function="ease-in-out")
 					template(#default='{ state, ...props }')
 						span(v-bind='props' class='w-full h-full bg-primary-500 dark:bg-primary-600 grow lg:hidden')
 
-							u-transition(delay='1.5s' before-enter-class="rotate-x-90" active-class="origin-bottom" duration='.5s' timing-function="ease-out")
+							u-transition(delay='1.65s' before-enter-class="rotate-x-90" active-class="origin-bottom" duration='.5s' timing-function="ease-out")
 								template(#default='{ state, ...props }')
 									beta-hero-image(v-bind='props' class='lg:order-1')
 
-			//- img(src='/api/favicon')
-			//- div(class='flex flex-col gap-8 items-start grow z-20 basis-3/4')
-				//- u-lazy(:delay='500' :transition class='grow flex w-full')
 			div(class='flex flex-col gap-4 lg:gap-8 grow w-full')
 				div(:class='ui.text.base' class="font-display uppercase font-bold text-6xl sm:text-8xl md:text-9xl lg:text-10xl xl:text-10xl")
-					h1(class='flex flex-wrap place-content-center place-items-center gap-x-4 sm:gap-x-6 transition-all duration-200') {{  t('im') }}
-						span(class='lg:order-1') {{ user.data?.name }},
+					u-transition(delay='1s' before-enter-class="max-w-0" enter-class='max-w-full' duration='1s' timing-function="ease-in-out")
+						template(#default='{ state, ...props }')
+							div(class='flex flex-wrap place-content-center place-items-center gap-x-3 transition-all duration-200' :class='[state.idle ? "sm:gap-x-3" : "sm:gap-x-6"]')
+								h1 {{  t('im') }}
+								h1(class='lg:order-1') {{ user.data?.name }},
 
-						u-transition(delay='1s' before-enter-class="max-w-0" enter-class='max-w-full' duration='1s' timing-function="ease-in-out")
-							template(#default='{ state, ...props }')
 								span(v-bind='props' class='bg-primary-500 dark:bg-primary-600 h-font grow')
 
-									u-transition(delay='1.75s' before-enter-class="scale-y-0" duration='.5s' timing-function="ease-in-out")
+									u-transition(delay='1.5s' before-enter-class="scale-y-0" duration='.5s' timing-function="ease-in-out")
 										template(#default='{ state, ...props }')
 											beta-hero-image(v-bind='props' class='lg:order-1 hidden lg:flex origin-bottom')
 
-					h1(class='flex place-content-center gap-x-4 sm:gap-x-6 hyphens-auto place-items-center') {{ user.data?.job }}
-						u-transition(delay='1s' before-enter-class="max-w-0" enter-class='max-w-full' duration='1s' timing-function="ease-in-out")
-							template(#default='{ state, ...props }')
-								span(v-bind='props' class='shrink grow lg:bg-primary-500 dark:lg:bg-primary-600 lg:-order-1 h-font')
-								//- span(v-bind='props' class='grow lg:hidden h-font')
+					u-transition(delay='1s' before-enter-class="max-w-0" enter-class='max-w-full' duration='1s' timing-function="ease-in-out")
+						template(#default='{ state, ...props }')
+							div(class='flex flex-wrap sm:flex-nowrap place-content-center gap-x-3 sm:gap-x-6 hyphens-auto place-items-center')
+								div(v-for='word, index in user.data?.job?.split(" ")' class='flex place-content-center place-items-center text-nowrap w-full sm:w-auto')
+									h1(class='shrink') {{ word }}
+									span(v-bind='props' class='grow lg:bg-primary-500 dark:lg:bg-primary-600 lg:-order-1 h-font sm:hidden')
+
+								span(v-bind='props' class='hidden sm:block shrink grow lg:bg-primary-500 dark:lg:bg-primary-600 lg:-order-1 h-font')
 
 			u-transition(delay='.25s' before-enter-class="opacity-0 translate-y-10" duration='.5s' timing-function="ease-out")
 				template(#default='{ state, ...props }')
@@ -53,13 +51,10 @@ div(class='bg-primary-100/20 dark:bg-primary-900/20 max-w-full overflow-clip fle
 							template(#default='{ state, ...props }')
 								span(v-bind='props' class='hidden lg:inline-block grow')
 
-						//- u-lazy(:transition class='max-w-sm')
 						div(v-bind='props' class='flex gap-y-3 gap-x-6 flex-col max-w-sm delay-[1.2s] sm:delay-0')
 							div(class='flex gap-y-3 gap-x-6 flex-wrap')
 								u-link-action(v-for='item in user.data?.portfolio' v-bind='item')
-
 								u-link-action(v-for='item, key in user.data?.contact' v-bind='item' :key)
-
 								u-link-action(:title='user.data?.location' icon='i-material-symbols:location-on-outline')
 </template>
 
