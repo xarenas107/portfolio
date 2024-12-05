bd1df8911963fc74caa3236b92acf4d9a26ee496
Xavier <xarenas107@gmail.com>
2024-12-05 09:44:58 -0500

    fix(color): solve wrong color accent display

--- app/plugins/color.ts
+++ app/plugins/color.ts
@@ -1,10 +1,14 @@
 export default defineNuxtPlugin(() => {
 	const app = useAppConfig()
-	const storage = useLocalStorage('color', () => 'orange')
+	const storage = useLocalStorage('color', () => '')
 
 	app.ui.primary = storage.value || app.ui.primary
 
 	watch(() => app.ui.primary, (value) => {
 		if (storage.value !== value) storage.value = value
+	})
+
+	onNuxtReady(() => {
+		if (!storage.value) app.ui.primary = 'orange'
 	})
 })
