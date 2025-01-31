969de4e0968fca45570b910b96f1d91811988a6a
Xavier <xarenas107@gmail.com>
2025-01-31 10:15:09 -0500

    feat: Add `project` page view

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,12 @@ beta-section-studies(v-intersect='intersect' ref='section')#studies
 beta-section-certificates(v-intersect='intersect' ref='section')#certificates
 beta-section-experiences(v-intersect='intersect' ref='section')#experience
 beta-section-skills(v-intersect='intersect' ref='section')#skills
-//- beta-section-projects
+
+div(class='relative w-full min-h-[200svh] flex items-center justify-center overflow-clip motion-reduce:hidden py-24 opacity-animation')
+	div(class="scale-animation rounded-full bg-primary-100/80 dark:bg-primary-900/80 p-8 aspect-square")
+		div(class="h-4 w-4 mask-animation mask-favicon bg-primary-500 dark:bg-primary-600 rounded-full relative")
+
+beta-section-projects(v-intersect='intersect' ref='section')#projects
 beta-section-footer
 </template>
 
@@ -23,14 +28,53 @@ const intersect = [
 			if (isIntersecting) hash.value = target.id
 		})
 	}, {
-		rootMargin: '10%',
-		threshold: 0.75
-	}]
+		rootMargin: '5%',
+		threshold: 0.25
+	}
+]
 </script>
 
 <style lang="scss">
-@use '@/assets/scss/styles';
-@use '@/assets/scss/transition';
-@use '@/assets/scss/scrollbar';
-@use '@/assets/scss/fonts';
+@keyframes scale {
+	from, 30% { scale: 1 }
+	to { scale: 100 }
+}
+
+@keyframes scale-full {
+	from, 20% { scale: 0 }
+	to { scale: 10 }
+}
+
+@keyframes opacity {
+	from { background: transparent }
+}
+
+@media (prefers-reduced-motion: no-preference) {
+	.mask-animation {
+		animation: scale-full ease both;
+		animation-timeline: view();
+		animation-range: 0svh 150svh
+	}
+
+	.scale-animation {
+		animation: scale ease both;
+		animation-timeline: view();
+		animation-range: 0svh 100svh
+	}
+
+	.opacity-animation {
+		animation: opacity linear both;
+		animation-timeline: view();
+		animation-range: 100svh 150svh;
+	}
+}
+
+.mask-favicon {
+	--logo: url('/api/favicon');
+	-webkit-mask-image: var(--logo);
+	mask-image: var(--logo);
+	mask-repeat: no-repeat;
+	mask-origin: center;
+	mask-position: center center;
+}
 </style>
--- app/pages/project.vue
+++ app/pages/project.vue
@@ -1,13 +1,39 @@
 <template lang="pug">
-nuxt-page
+nuxt-layout(v-bind='data' :name :pending) {{ t('project.overview.name') }}
 </template>
 
 <script lang="ts" setup>
+import type { LayoutKey } from '#build/types/layouts'
+
+type Project = {
+	id?: number
+	title?: string
+	description?: string
+	subtitle?: string
+	image?: string
+	layout: string
+}
+
 definePageMeta({
-	redirect: '/project/:id()',
 	navbar: 'back'
 })
+
+const { t } = useI18n()
+const route = useRoute()
+const { data, status } = useFetchContent<Project>(`project/${route.query.id}`, {
+	default: () => ({
+		title: '',
+		layout: 'case-study',
+		items: []
+	})
+})
+
+const name = computed(() => {
+	return `project-${data.value?.layout}` as LayoutKey
+})
+
+const { pending } = useStatus(status)
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 </style>
