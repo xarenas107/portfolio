936c43bc4790dec4ea23c3d170de8246f8c37b36
Xavier <xarenas107@gmail.com>
2025-05-07 09:37:15 +0200

    chore(fonts): Increase speed by loading local first

--- config/app.ts
+++ config/app.ts
@@ -4,11 +4,7 @@ export default <NuxtConfig['app']>{
 	head: {
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', sizes: '48x48', media: '(prefers-color-scheme: light)' },
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon-dark.ico', sizes: '48x48', media: '(prefers-color-scheme: dark)' },
-
-			// Preconnect and load font types
-			{ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-			{ rel: 'preconnect', href: 'https://fonts.googleapis.com' }
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon-dark.ico', sizes: '48x48', media: '(prefers-color-scheme: dark)' }
 		],
 		rootAttrs: {
 			class: 'bg-(--ui-bg)'
