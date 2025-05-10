2e7674cb8d637120cc4e924cec88a7833e03ab2d
Xavier <xarenas107@gmail.com>
2025-05-10 22:07:45 +0200

    chore(seo): enable `nuxt-seo-utils`

--- app/plugins/site.server.ts
+++ app/plugins/site.server.ts
@@ -1,9 +1,9 @@
 export default defineNuxtPlugin({
 	enforce: 'pre',
 	setup() {
-		// const app = useAppConfig()
-		// updateSiteConfig({
-		// 	name: app.name
-		// })
+		const app = useAppConfig()
+		updateSiteConfig({
+			name: app.name
+		})
 	}
 })
