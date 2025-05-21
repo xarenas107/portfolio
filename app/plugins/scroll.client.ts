784bbdac6ec24df040b13bbcf0b0e1efab7e0bb6
Xavier <xarenas107@gmail.com>
2025-05-21 16:30:10 +0200

    fix(scroll)> prevent when undefined hash

--- app/plugins/scroll.client.ts
+++ app/plugins/scroll.client.ts
@@ -1,5 +1,6 @@
 export default defineNuxtPlugin((nuxt) => {
 	const hash = useState<string>('to:hash')
+	if (!hash.value) return
 
 	nuxt.hook('page:finish', () => {
 		const el = document.querySelector(hash.value)
