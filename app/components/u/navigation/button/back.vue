dd3e377a95a7a8f1bbb6159e25e24bce019a6d6b
Xavier <xarenas107@gmail.com>
2025-06-03 16:28:12 +0200

    fix(back): back button replaced with `back to home`

--- app/components/u/navigation/button/back.vue
+++ app/components/u/navigation/button/back.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
-u-button(@click='router.back()' :aria-label='t("back.sr")' icon='heroicons:arrow-left-solid' type="button" color="neutral" variant='ghost' size='lg' class='aspect-square hover:bg-neutral-500/10 hover:dark:bg-neutral-400/10')
+u-button(@click='action()' :aria-label='label' icon='heroicons:arrow-left-solid' type="button" color="neutral" variant='ghost' size='lg' class='hover:bg-neutral-500/10 hover:dark:bg-neutral-400/10' :label)
 </template>
 
 <script setup lang="ts">
-const router = useRouter()
 const { t } = useI18n()
+
+const label = computed(() => t('back.home'))
+const action = () => navigateTo({ name: 'index', hash: '#portfolio' })
 </script>
 
 <style scoped>
