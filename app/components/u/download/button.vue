c755d4666e4930538373c70577a2b632ea3e043c
Xavier <xarenas107@gmail.com>
2025-06-06 20:32:13 +0200

    fix(download): solve wrong function

--- app/components/u/download/button.vue
+++ app/components/u/download/button.vue
@@ -1,13 +1,12 @@
 <template lang='pug'>
 u-tooltip(:text)
-	u-button(@click='toggle' :label :aria-label='text' icon="i-heroicons:arrow-down-tray-20-solid"  variant='ghost' color='neutral' size='lg' :class='[{ "aspect-square place-content-center": square }, "hover:bg-inverted/10 cursor-pointer focus-visible:bg-transparent focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"]')
+	u-button(v-if='data.resume?.href' :label :href='data.resume.href' :aria-label='text' :download="data.resume.name" icon="i-heroicons:arrow-down-tray-20-solid" variant='ghost' color='neutral' size='lg' :class='{ "aspect-square place-content-center": square }' class='"hover:bg-inverted/10 cursor-pointer focus-visible:bg-transparent focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"' as="a" loading-auto)
 </template>
 
 <script lang='ts' setup>
 type Props = {
 	type?: 'button'
 	showLabel?: boolean
-	// class?: string | string[]
 	square?: boolean
 }
 
@@ -19,17 +18,7 @@ const { t } = useI18n()
 
 const text = computed(() => t('resume.download'))
 const label = computed(() => props.showLabel ? text.value : undefined)
-const { origin } = useRequestURL()
 const { data } = useUser()
-const { share } = useShare()
-
-const toggle = () => {
-	share({
-		url: origin,
-		title: data.value?.name,
-		text: data.value?.job
-	})
-}
 </script>
 
 <style lang='scss' scoped>
