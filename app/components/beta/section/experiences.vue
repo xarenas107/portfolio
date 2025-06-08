b5cf02c0063d6b92c9f18899628c4d24a31ecc5c
Xavier <xarenas107@gmail.com>
2025-06-08 13:45:41 +0200

    fix(ui): adjust scroll-driven animations

--- app/components/beta/section/experiences.vue
+++ app/components/beta/section/experiences.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
-div(class='overflow-clip bg-(--ui-primary) relative flex motion-safe:min-h-screen flex-col')
-	div(class='h-64 w-full bg-(--ui-primary) flex ')
+div(class='overflow-clip bg-primary relative flex motion-safe:min-h-screen flex-col')
+	div(class='h-64 w-full bg-primary flex')
 		u-container(v-bind='options' class='h-full w-full origin-bottom items-end flex')
 			section-title(:class='ui.title' class="scroll-slide-animation" hyphens) {{  t('section.experience', 2) }}
 
 	div(class='relative flex w-full bg-default h-full motion-safe:min-h-[150vh] py-24 grow motion-reduce:overflow-auto scroll-hidden')
-		div(v-bind='options' class='px-4 sm:px-6 motion-reduce:pr-0 motion-reduce:sm:pr-0 motion-reduce:lg:pr-0 lg:px-8 gap-8 motion-reduce:max-w-7xl mx-auto w-full grow ')
+		div(v-bind='options' class='px-4 sm:px-6 motion-reduce:pr-0 motion-reduce:sm:pr-0 motion-reduce:lg:pr-0 max-width-animation max-w-7xl lg:px-8 gap-8 mx-auto w-full grow')
 
 			u-timeline(
 				:ui='ui.timeline'
@@ -46,10 +46,10 @@ const { data, pending } = useExperiences()
 
 const style = computed(() => {
 	const size = data.value?.length || 0
-	const length = 25 * (8 + size)
+	const length = 24 * 2 * size
 
 	return {
-		'--width': `${length}rem`
+		'--width': `max(${length}rem, 200dvw)`
 	}
 })
 
@@ -77,11 +77,17 @@ const ui = {
 
 @keyframes slide-from-end {
   from {
-    transform: translate(80%);
+    transform: translate(0%);
   }
   to {
-    transform: translate(-45%);
+    transform: translate(-50%);
   }
+}
+
+@keyframes max-widtth {
+	to {
+		max-width: none
+	}
 }
 
 @media (prefers-reduced-motion: no-preference) {
@@ -94,7 +100,13 @@ const ui = {
   .scroll-slide-animation-reverse {
 	animation: slide-from-end cubic-bezier(.12,.48,.72,1) both;
     animation-timeline: view();
-    animation-range: 0% 80%;
+    animation-range: 40% 80%;
+  }
+
+  .max-width-animation {
+	animation: max-width cubic-bezier(.12,.48,.72,1) both;
+    animation-timeline: view();
+    animation-range:40% 80%;
   }
 }
 </style>
