0b1ec7d9ed3aeb138d38501c238c9eff71a92386
Xavier <xarenas107@gmail.com>
2025-08-31 18:11:26 +0200

    chore: restore removed dependencies and update app configuration

--- config/app.ts
+++ config/app.ts
@@ -1,18 +1,23 @@
 import type { NuxtConfig } from '@nuxt/schema'
 
+type ExtendConfig<T = {}> = {
+	$development: Partial<T>
+	$production: Partial<T>
+} & Partial<T>
+
 export default {
 	head: {
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', sizes: '48x48', media: '(prefers-color-scheme: light)' },
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon-dark.ico', sizes: '48x48', media: '(prefers-color-scheme: dark)' }
 		],
-		rootAttrs: {
+		htmlAttrs: {
 			class: 'bg-(--ui-bg)'
-		}
+		},
+		meta: [
+			{ name: 'description', content: 'Curriculum Vitae' }
+		]
 	},
-	meta: [
-		{ name: 'description', content: 'Curriculum Vitae' }
-	],
 	$development: {
 		head: {
 			link: [{ rel: 'icon', type: 'image/svg+xml', href: '/icons/favicon-dev.svg' }]
@@ -23,4 +28,4 @@ export default {
 			link: [{ rel: 'icon', type: 'image/svg+xml', href: '/icons/favicon-adaptative.svg' }]
 		}
 	}
-} satisfies Partial<NuxtConfig['app']>
+} satisfies ExtendConfig<NuxtConfig['app']>
